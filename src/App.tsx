4366ee3d6975ecd5429a191c8589d98fc26fc036
Lucas Macarini <contatolucasmacarini@gmail.com>
2026-06-23 13:02:02 -0300

    feat: props components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+interface ICardProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Card = (props: ICardProps) => {
+  return (
+    <div style={{ border: "1px solid black" }}>
+      <span>Title: {props.title}</span>
+
+      <div>{props.children}</div>
+
+      <div>Footer</div>
+    </div>
+  );
+};
+
 export function App() {
-  return <>Ola</>;
+  return (
+    <div>
+      Ola
+      <p>Card: </p>
+      <Card title="a">Morango</Card>
+    </div>
+  );
 }
