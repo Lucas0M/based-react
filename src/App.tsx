9d3fbb88bc8bea25bc9c50bf947611ae091576a3
Lucas Macarini <contatolucasmacarini@gmail.com>
2026-06-24 12:26:13 -0300

    feat: final methods to-do list

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import { useState } from "react";
 type List = {
   id: string;
   label: string;
+  complete: boolean;
 };
 
 export function App() {
   const [value, setValue] = useState("");
   const [list, setList] = useState<List[]>([
-    { id: "1", label: "Fazer Cafe" },
-    { id: "2", label: "Fazer Cafe" },
-    { id: "3", label: "Fazer Almoco" },
-    { id: "4", label: "Fazer Janta" },
+    { id: "1", label: "Fazer Cafe", complete: false },
+    { id: "2", label: "Fazer Cafe", complete: false },
+    { id: "3", label: "Fazer Almoco", complete: false },
+    { id: "4", label: "Fazer Janta", complete: false },
   ]);
 
   return (
@@ -25,7 +26,7 @@ export function App() {
         onClick={() => {
           setList([
             ...list,
-            { id: (list.length + 1).toString(), label: value },
+            { id: (list.length + 1).toString(), label: value, complete: false },
           ]);
           setValue("");
         }}
@@ -35,7 +36,31 @@ export function App() {
 
       <ol>
         {list.map((listItem) => (
-          <li key={listItem.id}>{listItem.label}</li>
+          <li key={listItem.id}>
+            {listItem.label}
+
+            {listItem.complete ? "Concluido" : ""}
+            <button
+              onClick={() =>
+                setList([
+                  ...list.map((item) => ({
+                    ...item,
+                    complete:
+                      item.id === listItem.id ? !item.complete : item.complete,
+                  })),
+                ])
+              }
+            >
+              {listItem.complete ? "Desconcluir" : "Concluir"}
+            </button>
+            <button
+              onClick={() =>
+                setList([...list.filter((item) => item.id !== listItem.id)])
+              }
+            >
+              Remover
+            </button>
+          </li>
         ))}
       </ol>
     </div>
